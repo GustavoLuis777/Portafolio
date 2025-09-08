5f3063a76599ec744f4655bc3570511aa0e2aebc
Gustavo Luis <160779736+GustavoLuis777@users.noreply.github.com>
2025-09-08 16:39:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+   var typed = new Typed("#typed", {
+      strings: ["Desarrollador Web", "Diseñador Frontend", "Freelancer"],
+      typeSpeed: 70,
+      backSpeed: 50,
+      loop: true
+    });
